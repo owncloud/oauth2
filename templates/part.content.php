9ff9474ef05e7d2f450cbcdf7faa2c04b85e5971
lukebier <Lukas@Lukas-MacBook-758.local>
2016-11-07 21:11:44 +0100

    Added a short test text
    .

---
+++ templates/part.content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ownCloud - oauth2
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Lukas Biermann
+ * @copyright Lukas Biermann
+ */
+?>
+<p>OAuth 2.0 Eingabemaske - Platzhalter</p>
+// Just a test.
