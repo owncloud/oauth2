dad5d01ba0d8e4255e5c594e505ad4236a709cad
lukebier <Lukas@Lukas-MacBook-758.local>
2016-11-07 21:10:13 +0100

    Cleaned unnecessary parts

--- js/script.js
+++ js/script.js
@@ -8,24 +8,5 @@
  * @copyright Jonathan Neugebauer 2016
  */
 
-(function ($, OC) {
 
-	$(document).ready(function () {
-		$('#hello').click(function () {
-			alert('Hello from your script file');
-		});
-
-		$('#echo').click(function () {
-			var url = OC.generateUrl('/apps/oauth2/echo');
-			var data = {
-				echo: $('#echo-content').val()
-			};
-
-			$.post(url, data).success(function (response) {
-				$('#echo-result').text(response.echo);
-			});
-
-		});
-	});
-
-})(jQuery, OC);
+}
