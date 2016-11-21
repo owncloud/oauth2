8624a7dc9b93c623e6d4ebfc769f38f420e30fc9
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2016-11-21 21:03:48 +0100

    Add dummy implementation of addClient()

--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -10,19 +10,21 @@
  */
 
 namespace OCA\OAuth2\Controller;
+use OCA\OAuth2\Db\Client;
 use OCA\OAuth2\Db\ClientMapper;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\RedirectResponse;
-use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IRequest;
 
 class SettingsController extends Controller {
 
     private $clientMapper;
+    private $userId;
 
-    public function __construct($AppName, IRequest $request, ClientMapper $mapper) {
+    public function __construct($AppName, IRequest $request, ClientMapper $mapper, $UserId) {
         parent::__construct($AppName, $request);
         $this->clientMapper = $mapper;
+        $this->userId = $UserId;
     }
 
     /**
@@ -34,10 +36,18 @@ class SettingsController extends Controller {
      *
      */
     public function addClient() {
-        $clients = $this->clientMapper->findAll();
-        echo var_dump($clients);
-        echo $_POST["name"].$_POST["redirect_uri"];
-        return new RedirectResponse('../settings/admin#oauth-2.0');
+        $client = new Client();
+        $client->setClientId('abc');
+        $client->setName($_POST['name']);
+        $client->setClientSecret('topSecret');
+        $client->setRedirectUri($_POST['redirect_uri']);
+        $client->setGrantTypes('access_code');
+        $client->setScope('files');
+        $client->setUserId($this->userId);
+
+        $this->clientMapper->insert($client);
+
+        return new RedirectResponse('../../settings/admin#oauth-2.0');
     }
 
 }
