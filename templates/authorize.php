db820b6ed25a1081e192fdf2602d5148833c40d2
lukebier <l_biermann@icloud.com>
2016-12-01 23:08:05 +0100

    Update authorize.php
    New layout for authorizing applications.

--- templates/authorize.php
+++ templates/authorize.php
@@ -1,10 +1,12 @@
 <div id="app">
     <div id="app-content">
-        <div id="app-content-wrapper">
+        <div id="app-content-wrapper";
+             style="position: relative;">
             <div
-                style="text-align: center; position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);">
-                <p><b>Do you really like to authorize the client "<?php p($_['client_name']) ?>"?</b></p>
-
+                style="text-align: center; position: absolute; top: 40%; left: 50%; transform: translateX(-50%) translateY(-50%);
+                border:1px solid #1e2d43; background-color:#f8f8f8; padding: 10px; margin: 15px;background-color:#f8f8f8">
+                <p><b>Do you really like to authorize the application "<?php p($_['client_name']) ?>"?</b></p>
+                <p><b>The application will gain access to your files and username.</b></p>
                 <form action="" method="post" name="form">
                     <button type="submit">Authorize</button>
                 </form>
