8302428ef3b38f966750c8d3a1352730f651cc81
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2016-12-05 20:39:32 +0100

    Update PageControllerTest

--- tests/Unit/Controller/PageControllerTest.php
+++ tests/Unit/Controller/PageControllerTest.php
@@ -24,6 +24,12 @@
 
 namespace OCA\OAuth2\Tests\Unit\Controller;
 
+use OCA\OAuth2\AppInfo\Application;
+use OCA\OAuth2\Controller\PageController;
+use OCA\OAuth2\Db\AuthorizationCodeMapper;
+use OCA\OAuth2\Db\Client;
+use OCA\OAuth2\Db\ClientMapper;
+use OCP\AppFramework\Http\RedirectResponse;
 use PHPUnit_Framework_TestCase;
 
 use OCP\AppFramework\Http\TemplateResponse;
@@ -31,22 +37,39 @@ use OCP\AppFramework\Http\TemplateResponse;
 
 class PageControllerTest extends PHPUnit_Framework_TestCase {
 
+	/** @var PageController $controller */
 	private $controller;
+
+	/** @var string $userId */
 	private $userId = 'john';
 
 	public function setUp() {
 		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
 
-		$this->controller = new PageController(
-			'oauth2', $request, $this->userId
-		);
+		$app = new Application();
+		$container = $app->getContainer();
+
+		/** @var ClientMapper $clientMapper */
+		$clientMapper = $container->query('ClientMapper');
+
+		$client = new Client();
+		$client->setId('testId7890');
+		$client->setSecret('topSecret123');
+		$client->setRedirectUri('https://www.example.org');
+		$client->setName('Example');
+		$clientMapper->insert($client);
+
+		/** @var AuthorizationCodeMapper $authorizationCodeMapper */
+		$authorizationCodeMapper = $container->query('AuthorizationCodeMapper');
+
+		$this->controller = new PageController('oauth2', $request, $clientMapper, $authorizationCodeMapper, $this->userId);
 	}
 
-	public function testIndex() {
-		$result = $this->controller->index();
+	public function testAuthorize() {
+		$result = $this->controller->authorize('code', 'client_id', 'redirect_uri', 'state');
+		$this->assertTrue($result instanceof RedirectResponse);
 
-		$this->assertEquals(['user' => 'john'], $result->getParams());
-		$this->assertEquals('main', $result->getTemplateName());
+		$result = $this->controller->authorize('code', 'testId7890', urldecode('https://www.example.org'), 'state');
 		$this->assertTrue($result instanceof TemplateResponse);
 	}
 
