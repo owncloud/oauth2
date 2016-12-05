407e6dab66f562279a81773c7d05ff3c14f2659e
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2016-12-05 20:58:32 +0100

    Add registration of mappers

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -24,6 +24,10 @@
 
 namespace OCA\OAuth2\AppInfo;
 
+use OCA\OAuth2\Db\AccessTokenMapper;
+use OCA\OAuth2\Db\AuthorizationCodeMapper;
+use OCA\OAuth2\Db\ClientMapper;
+use OCA\OAuth2\Db\RefreshTokenMapper;
 use \OCP\AppFramework\App;
 
 class Application extends App {
@@ -35,6 +39,32 @@ class Application extends App {
      */
     public function __construct(array $urlParams=array()){
         parent::__construct('oauth2', $urlParams);
+
+		$container = $this->getContainer();
+
+		$container->registerService('ClientMapper', function($c) {
+			return new ClientMapper(
+				$c->query('ServerContainer')->getDb()
+			);
+		});
+
+		$container->registerService('AuthorizationCodeMapper', function($c) {
+			return new AuthorizationCodeMapper(
+				$c->query('ServerContainer')->getDb()
+			);
+		});
+
+		$container->registerService('AccessTokenMapper', function($c) {
+			return new AccessTokenMapper(
+				$c->query('ServerContainer')->getDb()
+			);
+		});
+
+		$container->registerService('RefreshTokenMapper', function($c) {
+			return new RefreshTokenMapper(
+				$c->query('ServerContainer')->getDb()
+			);
+		});
     }
 
     /**
