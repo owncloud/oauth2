fc959dec9b091f3619af078ae0809b8883de2086
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2016-12-12 22:16:05 +0100

    Add UtilitiesTest

---
+++ tests/Unit/UtilitiesTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Lukas Biermann
+ * @author Nina Herrmann
+ * @author Wladislaw Iwanzow
+ * @author Dennis Meis
+ * @author Jonathan Neugebauer
+ *
+ * @copyright Copyright (c) 2016, Project Seminar "PSSL16" at the University of Muenster.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+namespace OCA\OAuth2\Tests\Unit;
+
+use OCA\OAuth2\Utilities;
+use PHPUnit_Framework_TestCase;
+
+class PageControllerTest extends PHPUnit_Framework_TestCase {
+
+	public function testGenerateRandom() {
+		$random = Utilities::generateRandom();
+
+		$this->assertEquals(64, strlen($random));
+		$this->assertFalse(strpos($random, '+'));
+		$this->assertFalse(strpos($random, '/'));
+	}
+
+}
