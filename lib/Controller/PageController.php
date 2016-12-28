fd2ff09c282b132f6859c06910a943e54a29eb7c
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2016-12-28 16:13:30 +0100

    Add PHPDoc in PageController

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -49,15 +49,15 @@ class PageController extends Controller {
     /** @var string */
     private $userId;
 
-    /**
-     * PageController constructor.
-     * @param string $AppName
-     * @param IRequest $request
-     * @param ClientMapper $clientMapper
-	 * @param AuthorizationCodeMapper $authorizationCodeMapper
-     * @param string $UserId
-     */
-	public function __construct($AppName, IRequest $request, ClientMapper $clientMapper, AuthorizationCodeMapper $authorizationCodeMapper, $UserId){
+	/**
+	 * PageController constructor.
+	 * @param string $AppName The name of the app.
+	 * @param IRequest $request The request.
+	 * @param ClientMapper $clientMapper The client mapper.
+	 * @param AuthorizationCodeMapper $authorizationCodeMapper The authorization code mapper.
+	 * @param string $UserId The user ID.
+	 */
+	public function __construct($AppName, IRequest $request, ClientMapper $clientMapper, AuthorizationCodeMapper $authorizationCodeMapper, $UserId) {
 		parent::__construct($AppName, $request);
 
         $this->clientMapper = $clientMapper;
