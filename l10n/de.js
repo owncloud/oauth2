9d702920552e864dd38fadde82f5fae07c6c9484
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-06 14:45:28 +0100

    [tx-robot] updated from transifex

--- l10n/de.js
+++ l10n/de.js
@@ -1,25 +1,24 @@
 OC.L10N.register(
-	'oauth2',
-	{
-		"Do you really like to authorize the application “": "Wollen Sie wirklich die Applikation „",
-		"”?": "“ autorisieren?",
-		"The application will gain access to your username and will be allowed to manage files, folders and shares." : "Die Applikation wird Zugriff auf Ihren Benutzernamen erhalten und wird Dateien, Ordner und Freigaben verwalten dürfen.",
-		"Authorize" : "Autorisieren",
-		"Cancel" : "Abbrechen",
-		"OAuth 2.0": "OAuth 2.0",
-		"Registered clients": "Registrierte Clients",
-		"No clients registered." : "Es wurden keine Clients registriert.",
-		"Name" : "Name",
-		"Redirect URI" : "Redirect URI",
-		"Client Identifier" : "Client Identifier",
-		"Secret" : "Secret",
-		"Subdomains allowed" : "Subdomains erlaubt",
-		"Add client" : "Client hinzufügen",
-		"Allow subdomains" : "Subdomains erlauben",
-		"Add": "Hinzufügen",
-		"Authorized Applications" : "Autorisierte Applikationen",
-		"No applications authorized." : "Es wurden keine Applikationen autorisiert.",
-		"Are you sure you want to delete this item?" : "Wollen Sie diesen Eintrag wirklich löschen?"
-	},
-	'nplurals=2; plural=(n != 1);'
-);
+    "oauth2",
+    {
+    "Do you really like to authorize the application “" : "Möchten Sie die Anwendung \"",
+    "”?" : "\" wirklich autorisieren?",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Die Anwendung erhält Zugriff auf Ihren Benutzernamen und die Erlaubnis, Ihre Dateien, Ordner und geteilten Inhalte zu verwalten.",
+    "Authorize" : "Autorisieren",
+    "Cancel" : "Abbrechen",
+    "OAuth 2.0" : "OAuth 2.0",
+    "Registered clients" : "Angelegte Clients",
+    "No clients registered." : "Keine Clients angelegt.",
+    "Name" : "Name",
+    "Redirect URI" : "Weiterleitungs-URI",
+    "Client Identifier" : "Client ID",
+    "Secret" : "Secret",
+    "Subdomains allowed" : "Subdomains erlaubt",
+    "Are you sure you want to delete this item?" : "Möchten Sie das Objekt wirklich löschen?",
+    "Add client" : "Client hinzufügen",
+    "Allow subdomains" : "Erlaube Subdomains",
+    "Add" : "Hinzufügen",
+    "Authorized Applications" : "Autorisierte Anwendungen",
+    "No applications authorized." : "Keine Anwendungen autorisiert."
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/pt_BR.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Do you really like to authorize the application “" : "Você realmente gostaria de autorizar a aplicação \"",
+    "”?" : "”?",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "O aplicativo ganhará acesso ao seu nome de usuário e terá permissão para gerenciar arquivos, pastas e compartilhamentos.",
+    "Authorize" : "Autorizar ",
+    "Cancel" : "Cancelar",
+    "OAuth 2.0" : "OAuth 2.0",
+    "Registered clients" : "Clientes registados",
+    "No clients registered." : "Nenhum cliente cadastrado.",
+    "Name" : "Nome",
+    "Redirect URI" : "Redirecionar URI",
+    "Client Identifier" : "Identificador do cliente",
+    "Secret" : "Segredo",
+    "Subdomains allowed" : "Subdomínios permitidos",
+    "Are you sure you want to delete this item?" : "Tem certeza de que deseja excluir este item?",
+    "Add client" : "Adicionar cliente",
+    "Allow subdomains" : "Permitir subdomínios",
+    "Add" : "Adicionar",
+    "Authorized Applications" : "Aplicações Autorizadas",
+    "No applications authorized." : "Nenhum aplicação autorizada."
+},
+"nplurals=2; plural=(n > 1);");
