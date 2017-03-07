9972131a2a4b5d6b68c849119ec80b94b009d21d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-08 00:44:31 +0100

    [tx-robot] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Do you really like to authorize the application “" : "האם באמת לאשר את היישום “",
+    "”?" : "”?",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "היישום יקבל גישה לשם המשתמש שלך ויהיה רשאי לנהל קבצים, תיקיות ושיתופים.",
+    "Authorize" : "הרשאה",
+    "Cancel" : "ביטול",
+    "OAuth 2.0" : "OAuth 2.0",
+    "Registered clients" : "לקוחות רשומים",
+    "No clients registered." : "אין לקוחות רשומים.",
+    "Name" : "שם",
+    "Redirect URI" : "הפניה מחדש של URI",
+    "Client Identifier" : "מזהה לקוח",
+    "Secret" : "סוד",
+    "Subdomains allowed" : "תיקיות משנה מאופשרות",
+    "Are you sure you want to delete this item?" : "האם באמת למחוק פריט זה?",
+    "Add client" : "הוספת לקוח",
+    "Allow subdomains" : "לאפשר תיקיות משנה",
+    "Add" : "הוספה",
+    "Authorized Applications" : "הרשאת יישום",
+    "No applications authorized." : "אין יישומים מאושרים."
+},
+"nplurals=2; plural=(n != 1);");
