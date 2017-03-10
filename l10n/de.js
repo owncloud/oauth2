2fea859b2a99d09d89a418abe455672e382e0011
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-10 02:08:16 +0100

    [tx-robot] updated from transifex

--- l10n/de.js
+++ l10n/de.js
@@ -1,9 +1,9 @@
 OC.L10N.register(
     "oauth2",
     {
-    "Do you really like to authorize the application “" : "Möchtest du die Anwendung \"",
+    "Do you really like to authorize the application “" : "Möchten Sie die Anwendung \"",
     "”?" : "\" wirklich autorisieren?",
-    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Die Anwendung erhält Zugriff auf deinen Benutzernamen und die Erlaubnis, deine Dateien, Ordner und geteilten Inhalte zu verwalten.",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Die Anwendung erhält Zugriff auf Ihren Benutzernamen und die Erlaubnis, Ihre Dateien, Ordner und geteilten Inhalte zu verwalten.",
     "Authorize" : "Autorisieren",
     "Cancel" : "Abbrechen",
     "OAuth 2.0" : "OAuth 2.0",
@@ -14,7 +14,7 @@ OC.L10N.register(
     "Client Identifier" : "Client ID",
     "Secret" : "Secret",
     "Subdomains allowed" : "Subdomains erlaubt",
-    "Are you sure you want to delete this item?" : "Möchtest du das Objekt wirklich löschen?",
+    "Are you sure you want to delete this item?" : "Möchten Sie das Objekt wirklich löschen?",
     "Add client" : "Client hinzufügen",
     "Allow subdomains" : "Subdomains erlauben",
     "Add" : "Hinzufügen",
---
+++ l10n/nb_NO.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Do you really like to authorize the application “" : "Vil du virkelig autorisere applikasjonen?",
+    "”?" : "?",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Applikasjonen vil få tilgang til ditt brukernavn og vil kunne administrere filer, lokale mapper og delte mapper.",
+    "Authorize" : "Autoriser",
+    "Cancel" : "Avbryt",
+    "OAuth 2.0" : "OAuth 2.0",
+    "Registered clients" : "Registrerte klienter",
+    "No clients registered." : "Ingen klienter er registrert",
+    "Name" : "Navn",
+    "Redirect URI" : "Omdirigerings URL",
+    "Client Identifier" : "Klient id",
+    "Secret" : "Secret",
+    "Subdomains allowed" : "Underdomener tillatt",
+    "Are you sure you want to delete this item?" : "Er du sikker på at du vil slette dette elementet?",
+    "Add client" : "Legg til klient",
+    "Allow subdomains" : "Tillat underdomener",
+    "Add" : "Legg til",
+    "Authorized Applications" : "Autoriser applikasjon",
+    "No applications authorized." : "Ingen applikasjoner er autorisert."
+},
+"nplurals=2; plural=(n != 1);");
