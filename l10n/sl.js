5a6bea210ec6332cd4ae9828637eb25be8ae4911
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-14 02:08:17 +0100

    [tx-robot] updated from transifex

---
+++ l10n/sl.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Do you really like to authorize the application “" : "Ali ste prepričani, da želite overiti program »",
+    "”?" : "”?",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Po overitvi pridobi program dovoljenje za uporabo uporabniškega imena, s čimer je omogočen dostop do upravljanja datotek, map in souporabe.",
+    "Authorize" : "Overi",
+    "Cancel" : "Prekliči",
+    "OAuth 2.0" : "OAuth 2.0",
+    "Registered clients" : "Vpisani odjemalci",
+    "No clients registered." : "Ni vpisanih odjemalcev.",
+    "Name" : "Ime",
+    "Redirect URI" : "Preusmeritveni naslov URI",
+    "Client Identifier" : "Določilo odjemalca",
+    "Secret" : "Skrivni ključ",
+    "Subdomains allowed" : "Uporaba poddomen je dovoljena",
+    "Are you sure you want to delete this item?" : "Ali ste prepričani, da želite izbrisati predmet?",
+    "Add client" : "Dodaj odjemalca",
+    "Allow subdomains" : "Dovoli poddomene",
+    "Add" : "Dodaj",
+    "Authorized Applications" : "Overjeni programi",
+    "No applications authorized." : "Ni overjenih programov."
+},
+"nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);");
