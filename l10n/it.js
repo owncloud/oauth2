6544afaddcdcfbdd0523c468c5dc559f614ef1bf
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-27 02:08:17 +0200

    [tx-robot] updated from transifex

---
+++ l10n/it.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Request not valid" : "Richiesta non valida",
+    "” if this error persists." : "” se questo errore permane.",
+    "Back" : "Indietro",
+    "Do you really like to authorize the application “" : "Vuoi davvero autorizzare l'applicazione “",
+    "”?" : "”?",
+    "Authorize" : "Autorizza",
+    "Cancel" : "Annulla",
+    "OAuth 2.0" : "OAuth 2.0",
+    "No clients registered." : "Nessun client registrato.",
+    "Name" : "Nome",
+    "Client Identifier" : "Identificatore client",
+    "Secret" : "Segreto",
+    "Subdomains allowed" : "Sottodomini consentiti",
+    "Are you sure you want to delete this item?" : "Sei sicuro di voler eliminare questo elemento?",
+    "Add client" : "Aggiungi client",
+    "Allow subdomains" : "Consenti sottodomini",
+    "Add" : "Aggiungi",
+    "Authorized Applications" : "Applicazioni autorizzate",
+    "No applications authorized." : "Nessuna applicazione autorizzata."
+},
+"nplurals=2; plural=(n != 1);");
