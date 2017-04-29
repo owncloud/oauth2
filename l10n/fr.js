17ab08025c3c7caf9874893c50947cc70aadb07f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-04-29 02:08:16 +0200

    [tx-robot] updated from transifex

---
+++ l10n/fr.js
@@ -0,0 +1,25 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Request not valid" : "Requête invalide",
+    "This request is not valid. Please contact the administrator if this error persists." : "Cette requête est invalide. Merci de contacter l'administrateur si l'erreur persiste.",
+    "This request is not valid. Please contact the administrator of “" : "Cette requête est invalide. Merci de contacter l'administrateur de \"",
+    "” if this error persists." : "\" si l'erreur persiste.",
+    "Back" : "Retour",
+    "Do you really like to authorize the application “" : "Voulez-vous vraiment autoriser l'application \"",
+    "”?" : "\" ?",
+    "Authorize" : "Autoriser",
+    "Cancel" : "Annuler",
+    "Registered clients" : "Clients enregistrés",
+    "No clients registered." : "Aucun client enregistré.",
+    "Name" : "Nom",
+    "Secret" : "Secret",
+    "Subdomains allowed" : "Sous-domaines autorisés",
+    "Are you sure you want to delete this item?" : "Êtes-vous sûr de vouloir supprimer cet élément ?",
+    "Add client" : "Ajouter un client",
+    "Allow subdomains" : "Autoriser un sous-domaine",
+    "Add" : "Ajouter",
+    "Authorized Applications" : "Applications autorisées",
+    "No applications authorized." : "Aucune application autorisée."
+},
+"nplurals=2; plural=(n > 1);");
