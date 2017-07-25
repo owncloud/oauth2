dd0049814da9f2e739dc3fdc0a1c3c35093bf655
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-25 01:44:14 +0000

    [tx-robot] updated from transifex

--- l10n/nl.js
+++ l10n/nl.js
@@ -1,16 +1,12 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application \"{app}\" is requesting access to your account. To authorize it, please log in first." : "De applicatie \"{app}\" vraagt toegang tot je account. Om dit toe te staan, gelieve eerst aan te melden.",
     "Request not valid" : "Ongeldige aanvraag",
     "This request is not valid. Please contact the administrator if this error persists." : "De aanvraag is niet geldig. Neem contact op met de beheerder als de fout aanhoudt.",
-    "This request is not valid. Please contact the administrator of “" : "De aanvraag is niet geldig. Neem contact op met de beheerder van \"",
-    "” if this error persists." : "\" als de fout aanhoudt.",
     "Back" : "Terug",
-    "Do you really like to authorize the application “" : "Wil je echt applicatie “",
-    "”?" : "autoriseren”?",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "De applicatie krijgt toegang tot je gebruikersnaam en mag bestanden, mappen en shares beheren.",
     "Authorize" : "Toestaan",
-    "Cancel" : "Annuleren",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Geregistreerde clients",
     "No clients registered." : "Geen clients geregistreerd.",
@@ -24,6 +20,7 @@ OC.L10N.register(
     "Allow subdomains" : "Toestaan sub-domeinen",
     "Add" : "Toevoegen",
     "Authorized Applications" : "Geautoriseerde applicaties",
-    "No applications authorized." : "Geen applicaties geautoriseerd."
+    "No applications authorized." : "Geen applicaties geautoriseerd.",
+    "Logout and login as %s" : "Afmelden en aanmelden als 1%s"
 },
 "nplurals=2; plural=(n != 1);");
