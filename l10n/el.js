7657cc24659070317491f61e4c9a5f12dbfc2ab7
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-27 01:44:14 +0000

    [tx-robot] updated from transifex

--- l10n/el.js
+++ l10n/el.js
@@ -3,14 +3,9 @@ OC.L10N.register(
     {
     "Request not valid" : "Μη έγκυρη αίτηση",
     "This request is not valid. Please contact the administrator if this error persists." : "Μη έγκυρη αίτηση. Παρακαλώ επικοινωνήστε με τον διαχειριστή εάν το σφάλμα επιμένει.",
-    "This request is not valid. Please contact the administrator of “" : "Μη έγκυρη αίτηση. Παρακαλώ επικοινωνήστε με τον διαχειριστή του \"",
-    "” if this error persists." : "\" εάν το σφάλμα επιμένει.",
     "Back" : "Επιστροφή",
-    "Do you really like to authorize the application “" : "Θέλετε να πιστοποιήσετε την εφαρμογή \"",
-    "”?" : "”?",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Η εφαρμογή θα αποκτήσει πρόσβαση στο όνομα χρήστη σας και θα επιτρέπεται να διαχειριστεί αρχεία, φακέλους και κοινόχρηστα.",
     "Authorize" : "Πιστοποίησε",
-    "Cancel" : "Ακύρωση",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Εγγεγραμμένοι πελάτες",
     "No clients registered." : "Δεν έχουν εγγραφεί πελάτες.",
@@ -24,6 +19,7 @@ OC.L10N.register(
     "Allow subdomains" : "Να επιτρέπονται subdomains",
     "Add" : "Προσθήκη",
     "Authorized Applications" : "Πιστοποιημένες εφαρμογές",
-    "No applications authorized." : "Δεν έχουν πιστοποιηθεί εφαρμογές."
+    "No applications authorized." : "Δεν έχουν πιστοποιηθεί εφαρμογές.",
+    "Switch user" : "Εναλλαγή χρήστη"
 },
 "nplurals=2; plural=(n != 1);");
