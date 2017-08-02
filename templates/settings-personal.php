40d4d6da90246b336123114df4bc459146cd9d73
Thomas Müller <thomas.mueller@tmit.eu>
2017-08-02 11:34:31 +0200

    A user can only revoke his own authorized applications

--- templates/settings-personal.php
+++ templates/settings-personal.php
@@ -42,7 +42,7 @@ style('oauth2', 'main');
 			<tr>
 				<td><?php p($client->getName()); ?></td>
 				<td>
-					<form id="form-inline" class="delete" data-confirm="<?php p($l->t('Are you sure you want to delete this item?')); ?>" action="<?php p($_['urlGenerator']->linkToRoute('oauth2.settings.revokeAuthorization', ['id' => $client->getId()])); ?>?user_id=<?php p($_['user_id']); ?>" method="post">
+					<form id="form-inline" class="delete" data-confirm="<?php p($l->t('Are you sure you want to delete this item?')); ?>" action="<?php p($_['urlGenerator']->linkToRoute('oauth2.settings.revokeAuthorization', ['id' => $client->getId()])); ?>" method="post">
 						<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>" />
 						<input type="submit" class="button icon-delete" value="">
 					</form>
