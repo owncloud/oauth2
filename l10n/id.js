151612b26a55846c67de4744746d7ea4aaad9d68
ownCloud Jenkins <jenkins@owncloud.com>
2017-09-25 01:44:14 +0000

    [tx-robot] updated from transifex

--- l10n/id.js
+++ l10n/id.js
@@ -1,16 +1,13 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application was authorized successfully. You can now close this window." : "Aplikasi telah berhasil diotorisasi. Anda dapat menutup halaman ini sekarang",
     "Request not valid" : "Permintaan tidak valid",
     "This request is not valid. Please contact the administrator if this error persists." : "Permintaan tidak valid. Silakan hubungi administrator jika kesalahan ini terus berlanjut.",
-    "This request is not valid. Please contact the administrator of “" : "Permintaan tidak valid. Silakan hubungi administrator “",
-    "” if this error persists." : "” jika kesalahan ini terus berlanjut.",
     "Back" : "Kembali",
-    "Do you really like to authorize the application “" : "Apakah Anda ingin mengautentikasi aplikasi ini “",
-    "”?" : "”?",
+    "The “%s“ application would like permission to access your account" : "Aplikasi %s meminta ijin untuk mengakses akun anda",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Aplikasi ini akan memperoleh nama pengguna Anda dan akan diizinkan untuk mengelola berkas, folder dan berbagi.",
     "Authorize" : "Otorisasi",
-    "Cancel" : "Batal",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Klien terdaftar",
     "No clients registered." : "Tidak ada klien yang terdaftar.",
@@ -24,6 +21,9 @@ OC.L10N.register(
     "Allow subdomains" : "Izinkan subdomain",
     "Add" : "Tambah",
     "Authorized Applications" : "Aplikasi Terotorisasi",
-    "No applications authorized." : "Tidak ada aplikasi yang terotorisasi."
+    "No applications authorized." : "Tidak ada aplikasi yang terotorisasi.",
+    "Switch user" : "Ganti pengguna",
+    "You are logged as %s but the application requested access for user %s." : "Anda masuk sebagai %s tetapi aplikasi meminta akses untuk akun%s",
+    "Logout and login as %s" : "Keluar dan masuk sebagai %s"
 },
 "nplurals=1; plural=0;");
