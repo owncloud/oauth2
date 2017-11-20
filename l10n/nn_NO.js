c0826a05162155365b203bf8d5b81df4cf6edf6d
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-20 01:44:36 +0000

    [tx-robot] updated from transifex

--- l10n/nn_NO.js
+++ l10n/nn_NO.js
@@ -1,16 +1,20 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application \"{app}\" is requesting access to your account. To authorize it, please log in first." : "Programmet \"{app}\" spør om tilgang til din konto. Ver venleg å logge inn for å godkjenne tilgang.",
+    "The application was authorized successfully. You can now close this window." : "Programmet er godkjend for tilgang. Du kan stenge dette vindauget.",
+    "Request not valid" : "Førespurnad er ikkje gyldig",
+    "This request is not valid. Please contact the administrator if this error persists." : "Førespurnad er ikkje gyldig. Ver venleg å kontakte administrator om feilen oppstår att.",
+    "This request is not valid. Please contact the administrator of “%s” if this error persists." : "Førespurnad er ikkje gyldig. Ver venleg å kontakte administrator på “%s” om feilen oppstår att.",
     "Back" : "Tilbake",
-    "Do you really like to authorize the application “" : "Vil du autorisere applikasjon “",
-    "”?" : "”?",
+    "The “%s“ application would like permission to access your account" : "Programmet “%s” ber om tilgang til din konto",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Applikasjonen vil få tilgang til brukarnamn og kan administrere filer, katalogar og deling.",
     "Authorize" : "Autoriser",
-    "Cancel" : "Avbryt",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Registrerte klientar",
     "No clients registered." : "Ingen klientar registrert",
     "Name" : "Namn",
+    "Redirection URI" : "URI for ny adresse",
     "Client Identifier" : "Klient identifikator",
     "Secret" : "Hemmeleg",
     "Subdomains allowed" : "Under domene tillatt",
@@ -19,6 +23,9 @@ OC.L10N.register(
     "Allow subdomains" : "Tillat under domene",
     "Add" : "Legg til",
     "Authorized Applications" : "Autoriserte applikasjonar",
-    "No applications authorized." : "Ingen applikasjonar er autorisert"
+    "No applications authorized." : "Ingen applikasjonar er autorisert",
+    "Switch user" : "Bytt brukar",
+    "You are logged in as %s but the application requested access for user %s." : "Du er logga inn som %s men programmet ynskjer tilgang for brukar %s.",
+    "Switch users to continue" : "Bytt brukar for å fortsette"
 },
 "nplurals=2; plural=(n != 1);");
