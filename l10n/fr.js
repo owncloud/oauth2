a7966cd8a7cba05ec004eb220e1cf54c383f7721
ownCloud Jenkins <jenkins@owncloud.com>
2017-12-29 01:57:29 +0000

    [tx-robot] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -1,16 +1,14 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application \"{app}\" is requesting access to your account. To authorize it, please log in first." : "L'application \"{app}\" est nécessaire pour accéder à votre compte. Pour l'autoriser, veuillez d'abord vous connecter.",
+    "The application was authorized successfully. You can now close this window." : "L'application a été autorisée avec succès. Vous pouvez maintenant fermer la fenêtre.",
     "Request not valid" : "Requête invalide",
     "This request is not valid. Please contact the administrator if this error persists." : "Cette requête est invalide. Merci de contacter l'administrateur si l'erreur persiste.",
-    "This request is not valid. Please contact the administrator of “" : "Cette requête est invalide. Merci de contacter l'administrateur de \"",
-    "” if this error persists." : "\" si l'erreur persiste.",
+    "This request is not valid. Please contact the administrator of “%s” if this error persists." : "La requête n'est pas valide. Veuillez contacter votre administrateur de \"%s\" si l'erreur persiste.",
     "Back" : "Retour",
-    "Do you really like to authorize the application “" : "Voulez-vous vraiment autoriser l'application \"",
-    "”?" : "\" ?",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "L'application accédera à votre nom d’utilisateur et sera autorisée à gérer vos fichiers, dossiers et partages.",
     "Authorize" : "Autoriser",
-    "Cancel" : "Annuler",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Clients enregistrés",
     "No clients registered." : "Aucun client enregistré.",
@@ -24,6 +22,8 @@ OC.L10N.register(
     "Allow subdomains" : "Autoriser un sous-domaine",
     "Add" : "Ajouter",
     "Authorized Applications" : "Applications autorisées",
-    "No applications authorized." : "Aucune application autorisée."
+    "No applications authorized." : "Aucune application autorisée.",
+    "Switch user" : "Changer d'utilisateur",
+    "You are logged in as %s but the application requested access for user %s." : "Vous êtes connecté en tant que %s mais l'application nécessite l'accès de %s."
 },
 "nplurals=2; plural=(n > 1);");
