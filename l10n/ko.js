380ee09f64a84609dbc6a14402083d6aa9d5e78d
ownCloud Jenkins <jenkins@owncloud.com>
2018-01-06 01:44:48 +0000

    [tx-robot] updated from transifex

--- l10n/ko.js
+++ l10n/ko.js
@@ -1,16 +1,15 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application \"{app}\" is requesting access to your account. To authorize it, please log in first." : "\"{app}\" 앱에서 내 계정에 접근하려고 합니다. 인증하려면 먼저 로그인하십시오.",
+    "The application was authorized successfully. You can now close this window." : "앱을 인증했습니다. 이 창을 닫아도 됩니다.",
     "Request not valid" : "요청이 올바르지 않음",
     "This request is not valid. Please contact the administrator if this error persists." : "이 요청이 올바르지 않습니다. 오류가 계속 발생하면 관리자에게 연락하십시오.",
-    "This request is not valid. Please contact the administrator of “" : "이 요청이 올바르지 않습니다. 오류가 계속 발생하면 다음의 관리자에게 연락하십시오: “",
-    "” if this error persists." : "”.",
+    "This request is not valid. Please contact the administrator of “%s” if this error persists." : "이 요청이 올바르지 않습니다. 오류가 계속 발생하면 \"%s\" 관리자에게 연락하십시오.",
     "Back" : "뒤로",
-    "Do you really like to authorize the application “" : "다음 앱을 인증하시겠습니까: “",
-    "”?" : "”?",
+    "The “%s“ application would like permission to access your account" : "\"%s\" 앱에서 내 계정에 접근하려고 합니다",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "이 앱이 내 권한을 사용할 수 있으며, 파일, 폴더 및 공유를 관리할 수 있습니다.",
     "Authorize" : "인증",
-    "Cancel" : "취소",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "등록된 클라이언트",
     "No clients registered." : "등록된 클라이언트가 없습니다.",
@@ -24,6 +23,9 @@ OC.L10N.register(
     "Allow subdomains" : "하위 도메인 허용",
     "Add" : "추가",
     "Authorized Applications" : "인증된 앱",
-    "No applications authorized." : "인증된 앱이 없습니다."
+    "No applications authorized." : "인증된 앱이 없습니다.",
+    "Switch user" : "사용자 전환",
+    "You are logged in as %s but the application requested access for user %s." : "%s 님으로 로그인되어 있지만 앱에서 %s 사용자 접근을 요청했습니다.",
+    "Switch users to continue" : "계속하려면 사용자 전환"
 },
 "nplurals=1; plural=0;");
