daf8bddf57be888d966e3704381878394334f27d
ownCloud Jenkins <jenkins@owncloud.com>
2018-01-24 03:21:31 +0000

    [tx-robot] updated from transifex

--- l10n/is.js
+++ l10n/is.js
@@ -3,14 +3,9 @@ OC.L10N.register(
     {
     "Request not valid" : "Beiðni ekki gild",
     "This request is not valid. Please contact the administrator if this error persists." : "Þessi beiðni er ógild. Hafðu samband við kerfisstjóra ef þessi villa kemur áfram upp.",
-    "This request is not valid. Please contact the administrator of “" : "Þessi beiðni er ógild. Hafðu samband við kerfisstjóra “",
-    "” if this error persists." : "” ef þessi villa kemur áfram upp.",
     "Back" : "Til baka",
-    "Do you really like to authorize the application “" : "Viltu í alvörunni heimila forritið \"",
-    "”?" : "”?",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "Forritið mun fá aðgang að notandanafninu þínu og vera keyft að sýsla með skrár, möppur og sameignir.",
     "Authorize" : "Heimila",
-    "Cancel" : "Hætta við",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Skráð biðlaraforrit",
     "No clients registered." : "Engir biðlarar skráðir.",
@@ -24,6 +19,8 @@ OC.L10N.register(
     "Allow subdomains" : "Leyfa undirlén",
     "Add" : "Bæta við",
     "Authorized Applications" : "Heimiluð forrit",
-    "No applications authorized." : "Engin forrit heimiluð."
+    "No applications authorized." : "Engin forrit heimiluð.",
+    "Switch user" : "Skipta um notanda",
+    "Switch users to continue" : "Skiptu um notanda til að halda áfram"
 },
 "nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
