eed04cbe1ef14289989ae91faf0a46ed64bbab26
ownCloud Jenkins <jenkins@owncloud.com>
2018-04-21 01:44:17 +0000

    [tx-robot] updated from transifex

--- l10n/it.js
+++ l10n/it.js
@@ -1,9 +1,13 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application \"{app}\" is requesting access to your account. To authorize it, please log in first." : "L'applicazione \"{app}\" richiede accesso al tuo account. Per autorizzarlo, effettuare il login prima.",
+    "The application was authorized successfully. You can now close this window." : "Applicazione autorizzata con successo. Puoi chiudere questa finestra.",
     "Request not valid" : "Richiesta non valida",
     "This request is not valid. Please contact the administrator if this error persists." : "Questa richiesta non e' valida. Per favore contatta l'Amministratore se l'errore persiste.",
+    "This request is not valid. Please contact the administrator of “%s” if this error persists." : "La richiesta non è valida. Contatta l'amministratore di \"%s\" se l'errore persiste.",
     "Back" : "Indietro",
+    "The “%s“ application would like permission to access your account" : "L'applicazione \"%s\" vorrebbe avere il permesso di accedere al tuo account.",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "L'applicazione accedera' al tuo nome utente e sara' autorizzata a gestire i file, le cartelle e le condivisioni.",
     "Authorize" : "Autorizza",
     "OAuth 2.0" : "OAuth 2.0",
@@ -20,6 +24,8 @@ OC.L10N.register(
     "Add" : "Aggiungi",
     "Authorized Applications" : "Applicazioni autorizzate",
     "No applications authorized." : "Nessuna applicazione autorizzata.",
-    "Switch user" : "Cambia utente"
+    "Switch user" : "Cambia utente",
+    "You are logged in as %s but the application requested access for user %s." : "Sei connesso come %s ma l'applicazione chiede l'accesso per l'utente %s.",
+    "Switch users to continue" : "Cambia utente per proseguire"
 },
 "nplurals=2; plural=(n != 1);");
