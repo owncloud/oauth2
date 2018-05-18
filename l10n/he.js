15156a8c395626ce984c2c682a6fabdebb5d7956
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-18 01:44:21 +0000

    [tx-robot] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -28,4 +28,4 @@ OC.L10N.register(
     "You are logged in as %s but the application requested access for user %s." : "הנך מחובר כ-  %s אבל היישום מבקש הרשאת גישה עבור משתמש %s.",
     "Switch users to continue" : "יש להחליף משתמש להמשך"
 },
-"nplurals=4; plural=(n==1 ? 1: n == 2 ? 2 : (n % 10 == 0 and n > 10) ? 4);");
+"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
