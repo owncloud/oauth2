f2094caf064f9e40849e111563096a6151c0e38a
Thomas Müller <thomas.mueller@tmit.eu>
2020-05-27 04:08:44 +0000

    [tx] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -1,13 +1,16 @@
 OC.L10N.register(
     "oauth2",
     {
+    "Deleting..." : "削除中…",
+    "Saving..." : "保存中...",
+    "Name must not be empty" : "名前は空にできません",
+    "Redirect URI must not be empty" : "リダイレクトURIは空にできません",
+    "Redirect URI must be a valid URL" : "リダイレクトURIは有効なURLである必要があります",
+    "Client id must be a number" : "クライアントidは数字である必要があります",
     "Request not valid" : "リクエストは無効です",
-    "Back" : "戻る",
-    "Do you really like to authorize the application “" : "このアプリケーションを許可することに問題ありませんか？",
-    "”?" : "\"?",
+    "You are logged in as %s." : "%sとしてログインしました。",
     "The application will gain access to your username and will be allowed to manage files, folders and shares." : "アプリケーションがあなたのユーザー名に対してアクセスを得れば、ファイル、フォルダー、共有を管理できるようになります。",
     "Authorize" : "許可",
-    "Cancel" : "キャンセル",
     "OAuth 2.0" : "OAuth2.0",
     "Registered clients" : "登録クライアント",
     "No clients registered." : "クライアント未登録",
@@ -16,11 +19,12 @@ OC.L10N.register(
     "Client Identifier" : "クライアント識別子",
     "Secret" : "シークレットキー",
     "Subdomains allowed" : "サブドメイン許可",
-    "Are you sure you want to delete this item?" : "このアイテムを完全に削除しますか？",
     "Add client" : "クライアント追加",
     "Allow subdomains" : "サブドメイン許可",
     "Add" : "追加",
     "Authorized Applications" : "認可済みアプリケーション",
-    "No applications authorized." : "許可されたアプリケーションはありません。"
+    "No applications authorized." : "許可されたアプリケーションはありません。",
+    "Are you sure you want to delete this item?" : "このアイテムを完全に削除しますか？",
+    "Switch user" : "ユーザーの変更"
 },
 "nplurals=1; plural=0;");
