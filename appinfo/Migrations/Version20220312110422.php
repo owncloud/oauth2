9d481e57ec18e1c3c5096f3c8a3beb59adfccf12
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-03-14 11:20:17 +0100

    add missing token index
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ appinfo/Migrations/Version20220312110422.php
@@ -0,0 +1,19 @@
+<?php
+namespace OCA\oauth2\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use OCP\Migration\ISchemaMigration;
+
+/**
+ * This adds a unique index for token auth queries
+ */
+class Version20220312110422 implements ISchemaMigration {
+	public function changeSchema(Schema $schema, array $options) {
+		$prefix = $options['tablePrefix'];
+		$table = $schema->getTable("{$prefix}oauth2_access_tokens");
+		if (!$table->hasIndex('oauth2_token')) {
+			$table->addUniqueIndex(['token'], 'oauth2_token');
+		}
+	}
+}
