188b96b5014f17819b1588b426500806734b5f2f
ownClouders <devops@owncloud.com>
2022-09-13 00:44:12 +0000

    [tx] updated from transifex

--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -1,16 +1,18 @@
 OC.L10N.register(
     "oauth2",
     {
+    "The application \"{app}\" is requesting access to your account. To authorize it, please log in first." : "A aplicação \"{app}\" está a solicitar acesso à sua conta. Para autorizá-la, por favor, inicie a sessão primeiro.",
+    "Deleting..." : "A eliminar...",
+    "Saving..." : "A guardar...",
+    "Name must not be empty" : "Nome não deve estar em branco",
     "Request not valid" : "Pedido inválido",
     "This request is not valid. Please contact the administrator if this error persists." : "Este pedido não é válido. Por favor, contacte o administrador se o erro persistir.",
-    "This request is not valid. Please contact the administrator of “" : "Este pedido não é válido. Por favor, contacte o administrador de \"",
-    "” if this error persists." : "\" se este erro persistir.",
-    "Back" : "Voltar",
-    "Do you really like to authorize the application “" : "Tem mesmo a certeza que pretende autorizar a aplicação \"",
-    "”?" : "”?",
-    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "A aplicação terá acesso ao seu nome de utilizador e será permitido que controle ficheiros, pastas e partilhas.",
+    "This request is not valid. Please contact the administrator of “%s” if this error persists." : "Este pedido não é válido. Por favor, contacte o administrador do \"%s\" se este erro persistir.",
+    "The “%s“ application would like permission to access your account" : "A aplicação \"%s\" gostaria de permissão para aceder à sua conta",
+    "You are logged in as %s." : "Está autenticado como %s.",
+    "The application will gain access to your username and will be allowed to manage files, folders and shares." : "A aplicação terá acesso ao seu nome de utilizador e será permitido que controle os ficheiros, pastas e partilhas.",
     "Authorize" : "Autorizar",
-    "Cancel" : "Cancelar",
+    "Switch users to continue" : "Mude os utilizadores para continuar",
     "OAuth 2.0" : "OAuth 2.0",
     "Registered clients" : "Clientes registados",
     "No clients registered." : "Sem clientes registados.",
@@ -19,11 +21,14 @@ OC.L10N.register(
     "Client Identifier" : "Identificador de Cliente",
     "Secret" : "Segredo",
     "Subdomains allowed" : "Permitidos subdomínios",
-    "Are you sure you want to delete this item?" : "Tem a certeza que deseja eliminar este item?",
+    "Trusted client" : "Cliente confiável",
     "Add client" : "Adicionar cliente",
     "Allow subdomains" : "Permitir subdomínios",
     "Add" : "Adicionar",
     "Authorized Applications" : "Aplicações Autorizadas",
-    "No applications authorized." : "Sem aplicações autorizadas."
+    "No applications authorized." : "Sem aplicações autorizadas.",
+    "Are you sure you want to delete this item?" : "Tem a certeza que deseja eliminar este item?",
+    "Switch user" : "Mudar de utilizador",
+    "You are logged in as %s but the application requested access for user %s." : "Está autenticado como %s mas a aplicação solicitou acesso para o utilizador %s."
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
